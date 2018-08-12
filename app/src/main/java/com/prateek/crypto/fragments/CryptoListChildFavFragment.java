fa6cc5a682a203a990b7b683efac5665154803bb
pbprateek <pbprateek2@gmail.com>
2018-08-12 19:42:47 +0530

    Implimented swipe to refresh

--- app/src/main/java/com/prateek/crypto/fragments/CryptoListChildFavFragment.java
+++ app/src/main/java/com/prateek/crypto/fragments/CryptoListChildFavFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -46,6 +47,7 @@ public class CryptoListChildFavFragment extends Fragment {
     private ArrayList<Coin> coinList;
     private ArrayList<Coin> coinListFav;
     private FrameLayout root;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     public CryptoListChildFavFragment(){
 
@@ -63,6 +65,7 @@ public class CryptoListChildFavFragment extends Fragment {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_crypto_list_child_fav, container, false);
         mRecyclerView = view.findViewById(R.id.coin_list);
+        swipeRefreshLayout=view.findViewById(R.id.swipe_refresh);
         coinList=new ArrayList<Coin>();
         coinListFav=new ArrayList<Coin>();
         setList();
@@ -87,6 +90,13 @@ public class CryptoListChildFavFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         retrofit = NetworkClient.getClient().create(NetworkService.class);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                networkCall();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
         if(CoinListSingleton.getInstance().getList().size()==0){
             networkCall();
         }
@@ -163,6 +173,7 @@ public class CryptoListChildFavFragment extends Fragment {
         private TextView change1Text;
         private TextView change24Text;
         private TextView change7Text;
+        private TextView priceText;
 
         public CoinHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_item_coin, parent, false));
@@ -174,6 +185,7 @@ public class CryptoListChildFavFragment extends Fragment {
             change1Text = itemView.findViewById(R.id.change1);
             change7Text=itemView.findViewById(R.id.change7);
             change24Text = itemView.findViewById(R.id.change24);
+            priceText=itemView.findViewById(R.id.coinPrice);
         }
 
         public void bind(Coin coin, int pos) {
@@ -187,6 +199,7 @@ public class CryptoListChildFavFragment extends Fragment {
             change1Text.setText("1h "+mCoin.getPercentChange1h());
             change24Text.setText("24h "+mCoin.getPercentChange24h());
             change7Text.setText("7d "+mCoin.getPercentChange7d());
+            priceText.setText("Price: $ "+mCoin.getPriceUsd());
             button.setTag(pos);
             if (set.contains(mCoin.getId())) {
                 button.setChecked(true);
